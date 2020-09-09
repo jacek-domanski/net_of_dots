940c0fe0b627ab57783497e078e6f2579129d76d
jacek-domanski <j.domanski92@gmail.com>
2020-09-09 18:01:21 +0200

    :sparkles: Add empty canvas

--- netofdots.py
+++ netofdots.py
@@ -1,13 +1,16 @@
 from tkinter import *
 from time import sleep
+from parameters import *
 
 
-class NetOfDots:
+class NetOfDots(object):
     def __init__(self):
-        pass
+        self.root = Tk()
+        self.canvas = Canvas(self.root, width=CANVAS_WIDTH, height=CANVAS_HEIGHT)
+        self.canvas.pack()
 
     def run(self):
-        pass
+        self.root.mainloop()
 
 
 if __name__ == '__main__':
