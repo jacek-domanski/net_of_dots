f90380ef548a0c5cf36fc7327cc101c9f234e210
jacek-domanski <j.domanski92@gmail.com>
2020-09-09 18:32:44 +0200

    :sparkles: Random point appers

--- netofdots.py
+++ netofdots.py
@@ -1,13 +1,22 @@
 from tkinter import *
 from time import sleep
 from parameters import *
+from random import randint
 
 
 class NetOfDots(object):
     def __init__(self):
         self.root = Tk()
         self.canvas = Canvas(self.root, width=CANVAS_WIDTH, height=CANVAS_HEIGHT)
+        self.point1 = self._create_circle(self.canvas,
+                                          randint(POINT_RADIUS, CANVAS_WIDTH-POINT_RADIUS),
+                                          randint(POINT_RADIUS, CANVAS_WIDTH-POINT_RADIUS),
+                                          POINT_RADIUS,
+                                          fill='black')
         self.canvas.pack()
+
+    def _create_circle(self, canvas, x, y, r, **kwargs):
+        return canvas.create_oval(x-r, y-r, x+r, y+r, kwargs)
 
     def run(self):
         self.root.mainloop()
