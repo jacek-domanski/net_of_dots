8fc50cb91c37bb66d1b97296e0fa56e2724d5e1f
jacek-domanski <j.domanski92@gmail.com>
2020-09-09 17:49:41 +0200

    :sparkles: Add empty NetOfDots class

---
+++ netofdots.py
@@ -0,0 +1,15 @@
+from tkinter import *
+from time import sleep
+
+
+class NetOfDots:
+    def __init__(self):
+        pass
+
+    def run(self):
+        pass
+
+
+if __name__ == '__main__':
+    nod = NetOfDots()
+    nod.run()
