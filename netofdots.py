6ee0e22517e4c6aa4afc446018f55d7a0401a7e1
jacek-domanski <j.domanski92@gmail.com>
2020-09-09 19:02:05 +0200

    :sparkles: Much points, very moving

--- netofdots.py
+++ netofdots.py
@@ -8,24 +8,39 @@ class NetOfDots(object):
     def __init__(self):
         self.root = Tk()
         self.canvas = Canvas(self.root, width=CANVAS_WIDTH, height=CANVAS_HEIGHT)
-        self.point1 = self._create_circle(self.canvas,
-                                          randint(POINT_RADIUS, CANVAS_WIDTH-POINT_RADIUS),
-                                          randint(POINT_RADIUS, CANVAS_WIDTH-POINT_RADIUS),
-                                          POINT_RADIUS,
-                                          fill='black')
+        self.points = self._create_points()
+        self.x_velos = self.generate_random_velos()
+        self.y_velos = self.generate_random_velos()
         self.canvas.pack()
 
+    def _create_points(self):
+        points = []
+        for _ in range(NO_OF_POINTS):
+            point = self._create_circle(self.canvas,
+                                        randint(POINT_RADIUS, CANVAS_WIDTH - POINT_RADIUS),
+                                        randint(POINT_RADIUS, CANVAS_WIDTH - POINT_RADIUS),
+                                        POINT_RADIUS,
+                                        fill='black')
+            points.append(point)
+
+        return points
+
     def _create_circle(self, canvas, x, y, r, **kwargs):
-        return canvas.create_oval(x-r, y-r, x+r, y+r, kwargs)
+        return canvas.create_oval(x - r, y - r, x + r, y + r, kwargs)
 
     def animation(self):
-        x_velo = self._random_velo()
-        y_velo = self._random_velo()
-
         while True:
             sleep(REFRESH_TIME)
-            self.canvas.move(self.point1, x_velo, y_velo)
+            for point, x_velo, y_velo in zip(self.points, self.x_velos, self.y_velos):
+                self.canvas.move(point, x_velo, y_velo)
             self.canvas.update()
+
+    def generate_random_velos(self):
+        velos = []
+        for _ in range(NO_OF_POINTS):
+            velos.append(self._random_velo())
+
+        return velos
 
     def _random_velo(self):
         span = POINT_VELO_TO - POINT_VELO_FROM
